773d6e66bb991b7aac1ed9ed63f6bb39bf5c844a
ryangeor <ryangeor@mainbox>
2024-10-20 18:08:23 +0100

    init

--- keyboards/jlw/le_chiffre_32/keymaps/vial/keymap.c
+++ keyboards/jlw/le_chiffre_32/keymaps/vial/keymap.c
@@ -12,42 +12,77 @@
 #define HM_O	LGUI_T(KC_O)
 
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_3thumb(
-         KC_Q,   KC_W,   KC_F,   KC_P,   KC_B,        QK_BOOT,           KC_J,   KC_L,   KC_U,    KC_Y,   KC_QUOT,
-         HM_A,   HM_R,   HM_S,   KC_T,   KC_G,                   KC_M,   KC_N,   HM_E,    HM_I,   HM_O,
-         KC_Z,   KC_X,   KC_C,   KC_D,   KC_V,                   KC_K,   KC_H,   KC_COMM, KC_DOT, KC_SLSH,
-                         LT(3, KC_TAB), KC_RALT, SFT_T(KC_BSPC),          LT(1, KC_SPC), KC_LGUI, LT(2,KC_ENT)
-    ),
-
-    [1] = LAYOUT_3thumb(
-         KC_GRAVE, KC_F2,    XXXXXXX,  KC_F4,    KC_F5, _______,     KC_BSLS,  KC_MINUS, KC_EQUAL, KC_LBRC,  KC_RBRC,
-         KC_1,     KC_2,     KC_3,     KC_4,     KC_5,               KC_6,     KC_7,     KC_8,     KC_9,     KC_0,
-         DF(0),    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            DF(1),    XXXXXXX,  _______,  _______,  KC_SCLN,
-                                       _______,  _______,            _______,  _______, _______,  _______
-    ),
-
-    [2] = LAYOUT_3thumb(
-         XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  XXXXXXX,  KC_F11,   KC_F12,   XXXXXXX,  XXXXXXX,
-         KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,              KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,
-         DF(0),    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            DF(2),    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-                                       _______,  _______,            _______,  _______, _______,  _______
-    ),
-    [3] = LAYOUT_3thumb(
-         XXXXXXX,  XXXXXXX,  LSG(KC_4),   LSG(KC_S),   XXXXXXX,  _______,  KC_HOME,  XXXXXXX,  KC_UP,    XXXXXXX,  KC_PGUP,
-         KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LGUI,  XXXXXXX,            KC_END,   KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_PGDN,
-         DF(0),    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            DF(2),    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-                                       _______,  _______,            _______,  _______, _______,  _______
-    )
+enum custom_keycodes
+{
+    SMTD_KEYCODES_BEGIN = SAFE_RANGE,
+    CKC_A,
+    CKC_S,
+    CKC_D,
+    CKC_F,
+    CKC_J,
+    CKC_K,
+    CKC_L,
+    CKC_EN,
+    SMTD_KEYCODES_END,
 };
 
+#include "sm_td.h"
+
+void on_smtd_action(uint16_t keycode, smtd_action action, uint8_t tap_count)
+{
+    switch (keycode) {
+        // L
+        SMTD_MT(CKC_A, KC_A, KC_LEFT_GUI)
+        SMTD_MT(CKC_S, KC_S, KC_LEFT_ALT)
+        SMTD_MT(CKC_D, KC_D, KC_LEFT_CTRL)
+        SMTD_MT(CKC_F, KC_F, KC_LSFT)
+        // R
+        SMTD_MT(CKC_J, KC_A, KC_LEFT_GUI)
+        SMTD_MT(CKC_K, KC_S, KC_LEFT_ALT)
+        SMTD_MT(CKC_L, KC_D, KC_LEFT_CTRL)
+    }
+}
+
+
+const int _BASE = 0;
+const int _SYM = 1;
+const int _NUM = 2;
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         QK_BOOT,           KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,
+        CKC_A,   CKC_S,   CKC_D,   CKC_F,   KC_G,                            KC_H,    CKC_J,   CKC_K,    CKC_L,    KC_SCLN,
+        KC_Z,    KC_X,    KC_C,    KC_V,    KC_LALT,                            KC_B,    KC_N,    KC_M,  KC_RALT,  KC_RGUI,
+                                   KC_LSFT,MO(1),                           KC_SPC,  MO(2)
+    ),
+    [1] = LAYOUT(
+        LSFT(KC_EQUAL), LSFT(KC_7),     KC_LBRC,      KC_RBRC,      KC_SLSH,       KC_NO,            KC_BSLS,       KC_SCLN,       LSFT(KC_SCLN), LSFT(KC_4),     KC_BSPC,
+        KC_MINUS,       LSFT(KC_MINUS), LSFT(KC_LBRC), LSFT(KC_RBRC), KC_EQUAL,                         LSFT(KC_GRAVE),  KC_COMM,        KC_DOT,          LSFT(KC_5),     LSFT(KC_6),
+        LSFT(KC_3),     LSFT(KC_2),     LSFT(KC_9),       LSFT(KC_0),       LSFT(KC_1),                         LSFT(KC_BSLS), KC_QUOTE,        LSFT(KC_QUOTE),  KC_GRAVE,       KC_TRNS,
+                                                      KC_TRNS,          KC_TRNS,                          KC_TRNS,         KC_TRNS
+    ),
+    [2] = LAYOUT(
+        KC_ESCAPE,      KC_7,           KC_8,             KC_9,             KC_0,           KC_NO,            KC_SCLN,       KC_0,            KC_1,            KC_2,           KC_BSPC,
+        KC_TAB,         KC_4,           KC_5,             KC_6,             KC_EQUAL,                         KC_DOT,          KC_3,            KC_4,            KC_5,           KC_6,
+        KC_LCTL,        KC_1,           KC_2,             KC_3,             KC_BSLS,                        KC_COMM,        KC_7,            KC_8,            KC_9,           KC_ENTER,
+                                                          KC_TRNS,          KC_TRNS,                          KC_TRNS,         KC_TRNS
+    ),
+    [3] = LAYOUT(
+        KC_ESCAPE,      KC_7,           KC_8,             KC_9,             KC_0,           KC_NO,            KC_SCLN,       KC_0,            KC_1,            KC_2,           KC_BSPC,
+        KC_TAB,         KC_4,           KC_5,             KC_6,             KC_EQUAL,                         KC_DOT,          KC_3,            KC_4,            KC_5,           KC_6,
+        KC_LCTL,        KC_1,           KC_2,             KC_3,             KC_BSLS,                        KC_COMM,        KC_7,            KC_8,            KC_9,           KC_ENTER,
+                                                          KC_TRNS,          KC_TRNS,                          KC_TRNS,         KC_TRNS
+    ),
+};
+
+
 // Encoder Map
-    const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-        [0] =   { ENCODER_CCW_CW(S(KC_F2), KC_F2) },
-        [1] =   { ENCODER_CCW_CW(KC_VOLU, KC_VOLD) },
-        [2] =   { ENCODER_CCW_CW(_______, _______) },
-        [3] =   { ENCODER_CCW_CW(_______, _______) }
-    };
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] =   { ENCODER_CCW_CW(S(KC_F2), KC_F2) },
+    [1] =   { ENCODER_CCW_CW(KC_VOLU, KC_VOLD) },
+    [2] =   { ENCODER_CCW_CW(_______, _______) },
+    [3] =   { ENCODER_CCW_CW(_______, _______) }
+};
 
 // Combos disabled for Vial
 #ifndef VIAL_ENABLE
@@ -75,6 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #define MODS_G MODS_ACTIVE(GUI) + SHIFT_ACTIVE
 #define MODS_B MODS_ACTIVE(ALT) + SHIFT_ACTIVE
 
+
 const uint8_t PROGMEM layer_colors[][3] = {
     {RGB_OFF},
     {RGB_RED},
@@ -85,6 +121,16 @@ const uint8_t PROGMEM layer_colors[][3] = {
     {RGB_PINK},
     {RGB_TEAL}
 };
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+    if (!process_smtd(keycode, record)){
+        return false;
+    }
+    return true;
+}
+
+
 
 void set_rgb_matrix_indicators(uint8_t led_min, uint8_t led_max) {
     #if defined(RGB_LAYER_INDICATOR_ENABLE)
